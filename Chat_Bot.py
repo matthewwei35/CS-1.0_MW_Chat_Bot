271577d757e1264360af469f4fefbde2f5ea4207
Matthew Wei <matthewwei35@gmail.com>
2020-09-22 22:03:07 -0700

    Added chinese food and deleted comments

--- Chat_Bot.py
+++ Chat_Bot.py
@@ -4,25 +4,20 @@
 
 from random import choice
 
-#combine functions and conditionals to get a response from the bot
-def get_mood_bot_response(user_response): 
+def get_food_bot_response(user_response): 
+  bot_response_chinese = ['Hotpot!', 'Dumplings!', 'Peking Roasted Duck!', 'Chow Mein!']
 
-  #add some bot responses to this list
-  bot_response_happy = ["Yay!", "That's nice.", "Awesome!", "I'm glad!"]
-
-  if user_response == "happy":
-    #TODO: use choice to randomly return a response from the list
-    return choice(bot_response_happy)
+  if user_response == "chinese":
+    return choice(bot_response_chinese)
   else:
-    return "Thats cool! "
+    return "Come back for more! "
 
-print("Welcome to Mood Bot")
-print("Please enter how you are feeling")
+print("Welcome to Food Bot")
+print("This bot reccommends specfic dishes in a certain category of cuisine")
 
 user_response = ""
-#TODO: we want to keep repeating until the user enters "done" what should we put here?
+
 while user_response != "done":
-  user_response = input("How are you feeling today?: ")
-  #TODO: what goes here
-  bot_response = get_mood_bot_response(user_response)
+  user_response = input("What are you craving right now?: ")
+  bot_response = get_food_bot_response(user_response)
   print(bot_response)
